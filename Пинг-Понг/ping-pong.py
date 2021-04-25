860e16267d0323f8bf93ecb8706f97f778866bf5
PlatovDmitriy <82760400+PlatovDmitriy@users.noreply.github.com>
2021-04-25 15:55:41 +0300

    end of lesson 1

---
+++ Пинг-Понг/ping-pong.py
@@ -0,0 +1,43 @@
+from pygame import *
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+       sprite.Sprite.__init__(self)
+       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+    def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+class Player(GameSprite):
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.x < win_width - 80:
+            self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.x < win_width - 80:
+            self.rect.y += self.speed
+win_width = 650
+win_height = 500
+run = True
+player = Player('player.png', 10,10,10,100,5)
+finish = False
+background = transform.scale(image.load('bg.jpg'), (win_width, win_height))
+window = display.set_mode((win_width, win_height))
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+    if not finish:
+        window.blit(background,(0,0))
+        player.reset()
+        player.update_l()
+        display.update()
+
+    time.delay(50)
+    #производим движения спрайтов
